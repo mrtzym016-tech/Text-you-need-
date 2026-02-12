3cdb162a0ff6aef61b8feaba5755f050d5406c4e
mrtzym016-tech <mrtzym016@gmail.com>
2026-02-12 12:50:36 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+  siteName: "Human Replies Library",
+  siteEmail: "support@humanreplieslibrary.com",
+  enablePaywall: false,   // غيرها true إذا تريد تفعل الحماية
+  paywallMessage: "Upgrade to access premium content.",
+};
